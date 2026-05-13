f7b621fa2534593294790bc25c9b0c3f2cf5b397
Francisco Sanchez <francisco.z.sanchez@oracle.com>
2026-05-12 23:03:25 -0700

    Added prettier

--- src/pricer.ts
+++ src/pricer.ts
@@ -1,44 +1,49 @@
-export type Category = 'size' | 'creamer'; 
-export type Option = 'small' | 'medium' | 'large' | 'none' | 'dairy' | 'non-dairy'; 
-export type Price = number; 
+export type Category = 'size' | 'creamer';
+export type Option =
+  | 'small'
+  | 'medium'
+  | 'large'
+  | 'none'
+  | 'dairy'
+  | 'non-dairy';
+export type Price = number;
 
-export interface Pricer { 
-    /** 
-     * * Invoked each time the user makes a selection. 
-     * * No need to validate arguments, the caller validates the arguments before this function is invoked. 
-     * * @returns the _total_ price of the coffee so far given all the selections made 
-     */ 
-    
-    (category: Category, option: Option): Price 
+export interface Pricer {
+  /**
+   * * Invoked each time the user makes a selection.
+   * * No need to validate arguments, the caller validates the arguments before this function is invoked.
+   * * @returns the _total_ price of the coffee so far given all the selections made
+   */
+
+  (category: Category, option: Option): Price;
 }
 
 const priceMap: Record<Option, number> = {
-    small: 1.0,
-    medium: 1.50,
-    large: 2.0,
-    none: 0,
-    dairy: 0.25,
-    'non-dairy': 0.50,
-}
+  small: 1.0,
+  medium: 1.5,
+  large: 2.0,
+  none: 0,
+  dairy: 0.25,
+  'non-dairy': 0.5,
+};
 
-    
-    /** 
-     * A new pricer is created for each coffee being purchased. 
-     */ 
-export const createPricer = (): Pricer => { 
-    var size = 0;
-    var creamer = 0;
-    var price = 0;
+/**
+ * A new pricer is created for each coffee being purchased.
+ */
+export const createPricer = (): Pricer => {
+  var size = 0;
+  var creamer = 0;
+  var price = 0;
+  // your code goes here
+  return (category: Category, option: Option): Price => {
     // your code goes here
-    return (category: Category, option: Option): Price => {
-        // your code goes here
-        if(category == 'size') {
-            size = priceMap[option];
-        }
-        if (category == 'creamer') {
-            creamer = priceMap[option];
-        }
-        price = size + creamer;
-        return price;
+    if (category == 'size') {
+      size = priceMap[option];
     }
-}
+    if (category == 'creamer') {
+      creamer = priceMap[option];
+    }
+    price = size + creamer;
+    return price;
+  };
+};
--- tests/pricer.test.ts
+++ tests/pricer.test.ts
@@ -1,46 +1,46 @@
 import { it, expect, test } from '@jest/globals';
 import { createPricer } from 'pricer';
 
-it('provides the latest price given the options selected so far', () => { 
-    // starting a coffee order 
-    const pricer = createPricer(); 
-    
-    // set the default options 
-    pricer('size', 'small'); 
-    const defaultPrice = pricer('creamer', 'none'); 
-    expect(defaultPrice).toBe(1.00); 
-    
-    // user selects dairy creamer 
-    const priceAfterFirstSelection = pricer('creamer', 'dairy'); 
-    expect(priceAfterFirstSelection).toBe(1.25); 
-    
-    // user selects non-dairy creamer 
-    const priceAfterSecondSelection = pricer('creamer', 'non-dairy');
-    expect(priceAfterSecondSelection).toBe(1.50); 
-    
-    // user selects large 
-    const priceAfterThirdSelection = pricer('size', 'large');
-    expect(priceAfterThirdSelection).toBe(2.50);
+it('provides the latest price given the options selected so far', () => {
+  // starting a coffee order
+  const pricer = createPricer();
+
+  // set the default options
+  pricer('size', 'small');
+  const defaultPrice = pricer('creamer', 'none');
+  expect(defaultPrice).toBe(1.0);
+
+  // user selects dairy creamer
+  const priceAfterFirstSelection = pricer('creamer', 'dairy');
+  expect(priceAfterFirstSelection).toBe(1.25);
+
+  // user selects non-dairy creamer
+  const priceAfterSecondSelection = pricer('creamer', 'non-dairy');
+  expect(priceAfterSecondSelection).toBe(1.5);
+
+  // user selects large
+  const priceAfterThirdSelection = pricer('size', 'large');
+  expect(priceAfterThirdSelection).toBe(2.5);
 });
 
 const cases: [string, string, string, string, number][] = [
-  ['size', 'large', 'creamer', 'non-dairy', 2.50],
-  ['size', 'large', 'creamer', 'dairy',     2.25],
-  ['size', 'large', 'creamer', 'none',     2.00],
-  ['size', 'medium', 'creamer', 'non-dairy', 2.00],
-  ['size', 'medium', 'creamer', 'dairy',     1.75],
-  ['size', 'medium', 'creamer', 'none',     1.50],
-  ['size', 'small', 'creamer', 'non-dairy', 1.50],
+  ['size', 'large', 'creamer', 'non-dairy', 2.5],
+  ['size', 'large', 'creamer', 'dairy', 2.25],
+  ['size', 'large', 'creamer', 'none', 2.0],
+  ['size', 'medium', 'creamer', 'non-dairy', 2.0],
+  ['size', 'medium', 'creamer', 'dairy', 1.75],
+  ['size', 'medium', 'creamer', 'none', 1.5],
+  ['size', 'small', 'creamer', 'non-dairy', 1.5],
   ['size', 'small', 'creamer', 'dairy', 1.25],
-  ['size', 'small', 'creamer', 'none',      1.00],
+  ['size', 'small', 'creamer', 'none', 1.0],
 ];
 
 test.each(cases)(
   'size=%s value=%s, creamer=%s value=%s → expect $%s',
   (sizeKey, sizeVal, creamKey, creamVal, expected) => {
-    const pricer = createPricer()
-    pricer(sizeKey, sizeVal)
-    const result = pricer(creamKey, creamVal)
-    expect(result).toBe(expected)
-  }
-)
+    const pricer = createPricer();
+    pricer(sizeKey, sizeVal);
+    const result = pricer(creamKey, creamVal);
+    expect(result).toBe(expected);
+  },
+);
