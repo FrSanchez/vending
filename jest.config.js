55eb87036a0d2567f3fd2a8a08f51997f3517fc8
Francisco Sanchez <francisco.z.sanchez@oracle.com>
2026-05-12 22:34:40 -0700

    Blank canvas

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+   roots: ["<rootDir>/tests"],
+   preset: 'ts-jest',
+   testEnvironment: 'node',
+
+   // Recognize test file patterns
+  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+
+  // Allow importing from src without relative paths
+  moduleDirectories: ["node_modules", "src"],
+
+};
